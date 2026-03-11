18f7748429e1dcc7c288cf0b1b2db40ea087b558
Lingesh Sirpa <sirpalingesh9347@gmail.com>
2026-03-11 19:09:04 +0530

    Create Subarraywithequalnumberof(0's)and(1's).java

---
+++ JAVA/Hashing/Subarraywithequalnumberof(0's)and(1's).java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    map.put(0,-1);
+    int sum = 0; int maxLen = 0;
+    for(int i=0; i<nums.length; i++){
+       if(nums[i] == 0){
+        sum = sum-1;
+       }
+       else if(nums[i] == 1){
+        sum = sum+1;
+       }
+       if(map.containsKey(sum)){
+        int val = i-map.get(sum);
+        maxLen = Math.max(val, maxLen);
+       }else{
+        map.put(sum,i);
+       }
+    }
+    return maxLen;
+    }
+}
