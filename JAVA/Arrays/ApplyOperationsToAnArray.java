87ab057fa2183da503df1f7de09d378409159ba0
Lingesh Sirpa <sirpalingesh9347@gmail.com>
2026-08-10 15:24:58 +0530

    Create ApplyOperationsToAnArray.java

---
+++ JAVA/Arrays/ApplyOperationsToAnArray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i + 1]){
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int[] ans = new int[nums.length];
+        int j = 0;
+        for(int i : nums){
+            if(i != 0){
+              ans[j] = i;
+              j++;
+            }
+        }
+        while(j < ans.length){
+            ans[j] = 0;
+            j++;
+        }
+
+        return ans;
+    }
+}
